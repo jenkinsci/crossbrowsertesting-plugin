baab6fe1db6094070c8904c51bce858e59cf5ccd
Michael Hollister <hollister.michael@gmail.com>
2016-10-06 16:58:48 -0500

    shortened display name

--- src/main/java/org/jenkinsci/plugins/cbt_jenkins/CBTJenkinsBuildAction.java
+++ src/main/java/org/jenkinsci/plugins/cbt_jenkins/CBTJenkinsBuildAction.java
@@ -10,15 +10,17 @@ import hudson.model.Action;
 public class CBTJenkinsBuildAction implements Action {
 	private AbstractProject<?, ?> project;
     private AbstractBuild<?, ?> build;
+    private boolean isRunning = false;
 
+    
     private String displayName = null;
     private String testUrl = null;
     private String iconFileName = null;
-    
     private String testPublicUrl;
     private String testid;
     private String testtype;    
     public EnvVars environmentVariables;
+    
     
 
     @Override
@@ -30,12 +32,10 @@ public class CBTJenkinsBuildAction implements Action {
     public String getDisplayName() {
         return displayName;
     }
-
     @Override
     public String getUrlName() {
         return testUrl;
     }
-    
     public String getTestPublicUrl() {
     	return testPublicUrl;
     }
@@ -45,48 +45,54 @@ public class CBTJenkinsBuildAction implements Action {
     public String getTestId() {
     	return testid;
     }
-
     public int getBuildNumber() {
         return this.build.number;
     }
+    public AbstractBuild<?, ?> getBuild() {
+        return build;
+    }
+   
     public void setTestId(String id) {
     	this.testid = id;
     }
+    public void setDisplayName(String dn) {
+    	 // the displayname does not wrap after 49 characters
+    	 // so it will bleed into the view
+    	int maxCharactersViewable = 49;
+    	if (dn.length() > maxCharactersViewable - 3) {
+    		// going to cut the string down and add "..."
+    		dn = dn.substring(0, maxCharactersViewable - 3);
+    		dn += "...";
+    	}
+    	displayName = dn;
+    }
     public void setTestPublicUrl(String url) {
     	this.testPublicUrl = url;
-    }
-
-    public AbstractBuild<?, ?> getBuild() {
-        return build;
     }
     public void setTestUrl(String testUrl) {
     	//make it a little more url safe
     	this.testUrl = testUrl.replaceAll("[:.()|/ ]", "").toLowerCase();
     }
-    /*
-     * For Selenium
-     */
+    // For Selenium
     CBTJenkinsBuildAction(final String testtype, final EnvVars env, final boolean showLink, final AbstractBuild<?, ?> build) {
         this.testtype = testtype;
         this.environmentVariables = env;
         if (showLink) {
-        	this.displayName = "CBT Selenium Test (" + env.get("CBT_OPERATING_SYSTEM") + " " + env.get("CBT_BROWSER") + " " + env.get("CBT_RESOLUTION") + ")";
+        	setDisplayName("CBT Selenium Test (" + env.get("CBT_OPERATING_SYSTEM") + " " + env.get("CBT_BROWSER") + " " + env.get("CBT_RESOLUTION") + ")");
         	this.iconFileName = "/plugin/crossbrowsertesting/img/cbtlogo.png";
         	setTestUrl(displayName);
         }
         this.build = build;
     }
-    /*
-     * For Screenshots
-     */
+     // For Screenshots
     CBTJenkinsBuildAction(final String testtype, HashMap<String, String> ssInfo, final boolean showLink, final AbstractBuild<?, ?> build) {
     	this.testtype = testtype;
     	setTestId(ssInfo.get("screenshot_test_id"));
     	this.testPublicUrl = ssInfo.get("show_results_public_url");
     	if (showLink) {
-    		this.displayName = "CBT Screenshots Test (" + ssInfo.get("browser_list") + " " + ssInfo.get("url") + ")";
+    		setDisplayName("CBT Screenshots Test (" + ssInfo.get("browser_list") + " " + ssInfo.get("url") + ")");
     		this.iconFileName = "/plugin/crossbrowsertesting/img/cbtlogo.png";
-    		setTestUrl(displayName);
+    		setTestUrl(this.testid);
     	}
     	this.build = build;
     }
