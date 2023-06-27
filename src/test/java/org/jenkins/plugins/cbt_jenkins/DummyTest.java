8c09aba3b4177a8519e7db4be2bebf597ebc3d48
Alexander Brandes <mc.cache@web.de>
2023-06-27 20:43:37 +0200

    Add a dummy test

---
+++ src/test/java/org/jenkins/plugins/cbt_jenkins/DummyTest.java
@@ -0,0 +1,14 @@
+package org.jenkins.plugins.cbt_jenkins;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void anything() {
+        /*
+         * Intentionally blank. We just want a test that runs with JUnit so that buildPluginWithGradle() works
+         * in the Jenkinsfile.
+         */
+    }
+}
